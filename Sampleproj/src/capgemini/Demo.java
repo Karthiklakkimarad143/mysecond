41b38a39be56c69b40072f41edd10af0032155f9
Karthik lakkimarad <lakkimaradkarthik@gmail.com>
2022-05-15 13:51:31 +0530

    first commit added

---
+++ Sampleproj/src/capgemini/Demo.java
@@ -0,0 +1,11 @@
+package capgemini;
+
+public class Demo 
+{
+      public static void main(String[] args) 
+      {
+    	  System.out.println("hi capgemi");
+    	  System.out.println("welcome to all");
+		
+	  }
+}
